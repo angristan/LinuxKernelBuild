ac8b14129b657f15a756459d016cf66e4116f28a
Angristan <angristan@users.noreply.github.com>
2018-01-24 17:47:52 +0100

    Fixes and use make deb-pkg

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-# Variables
+# Get the Linux versions
 LINUX_VER=$1
 
 # Dependencies
 apt-get update
-apt-get install -y git fakeroot build-essential kernel-package ncurses-dev xz-utils libssl-dev bc liblz4-tool paxctl
+apt-get install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc liblz4-tool paxctl libelf-dev
 
 # GCC
 GCC_VER=$(gcc -dumpversion | cut -c1)
-if [[ $GCC_VER != "5" ]]; then
+if [[ $GCC_VER != "5" || $GCC_VER != "6" ]]; then
 	GCC_VER=$(gcc -dumpversion | cut -c1-3)
 fi
 apt-get install -y gcc-${GCC_VER}-plugin-dev
 
 # Download Linux source code
-cd /opt
-mkdir linux
-cd linux
+mkdir /opt/linux
+cd /opt/linux
 wget -c https://cdn.kernel.org/pub/linux/kernel/v$(echo $LINUX_VER | cut -c1).x/linux-${LINUX_VER}.tar.xz
 tar xvf linux-${LINUX_VER}.tar.xz
 cd linux-${LINUX_VER}
 
 # Config
 #cp /boot/config-$(uname -r) .config
+make menuconfig
 
 # Compilation
-make-kpkg clean
-make-kpkg --initrd --revision=$(date +%F) kernel_image kernel_headers -j $(nproc)
+make deb-pkg -j $(nproc)
 
 # Installation
 cd ..
-dpkg -i linux-image-${LINUX_VER}_$(date +%F)_amd64.deb
-dpkg -i linux-headers-${LINUX_VER}_$(date +%F)_amd64.deb
+dpkg -i linux-image-${LINUX_VER}_${LINUX_VER}-1_amd64.deb
+dpkg -i linux-headers-${LINUX_VER}_${LINUX_VER}-1_amd64.deb
+
+# Cleanup
+rm linux-${LINUX_VER}.tar.xz
+rm -r linux-${LINUX_VER}
+find . -type f -not -name "*.deb" -delete
 
 # Load the new kernel without reboot
 #apt-get install kexec-tools
